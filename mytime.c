7c488cb0fc62754b598bd3ca4476da9b0e8dd07c
Yang Liu <yliu158@binghamton.edu>
2017-12-01 15:48:09 -0500

    update

--- mytime.c
+++ mytime.c
@@ -11,54 +11,62 @@
 
 static int buffer_size;
 module_param(buffer_size, int, 0000);
+int queue* = NULL;
 
 MODULE_LICENSE("GPL");            ///< The license type -- this affects available functionality
 MODULE_AUTHOR("Yang Liu");    ///< The author -- visible when you use modinfo
 
-static ssize_t my_read(struct file *file, char __user * out, size_t size, loff_t * off) {
+static ssize_t pipe_read(struct file *file, char __user * out, size_t size, loff_t * off) {
+	
+	return 0;
+}
+
+static ssize_t pipe_write(struct file *filep, const char *buffer, size_t len, loff_t *offset) {
+	
+	
+	return 0;
+}
+
+static int pipe_open (struct inode * id, struct file * filep){
+	queue = (int*)kmalloc(size_of(int)*buffer_size, GFP_KERNEL);
+	if (queue == NULL) {
+		printk("Fail to allocate memory in kernel for the queue.\n");
+	}
+  printk(KERN_ALERT "Pipe has been opened.\n");
   return 0;
 }
 
-static ssize_t my_write(struct file *filep, const char *buffer, size_t len, loff_t *offset) {
-	  return 0;
-}
-
-static int my_open (struct inode * id, struct file * filep){
-  printk(KERN_ALERT "Char Device has been opened.\n");
+static int pipe_close(struct inode * id, struct file * filep) {
+  printk(KERN_ALERT "Pipe successfully closed.\n");
   return 0;
 }
 
-static int my_close(struct inode * id, struct file * filep) {
-  printk(KERN_ALERT "Char Device successfully closed.\n");
-  return 0;
-}
-
-static struct file_operations my_fops = {
+static struct file_operations pipe_fops = {
   .owner = THIS_MODULE,
-  .open = my_open,
+  .open = pipe_open,
   .release = my_close,
-  .read = my_read,
+  .read = pipe_read,
 };
 
-static struct miscdevice my_misc_device = {
+static struct miscdevice pipe = {
   .minor = MISC_DYNAMIC_MINOR,
-  .name = "mytime",
-  .fops = &my_fops
+  .name = "namedpipe",
+  .fops = &pipe_fops
 };
 
 
-static int __init my_init(void) {
-  printk(KERN_ALERT "Init mytime sucessfully.\n");
-  misc_register(&my_misc_device);
+static int __init pipe_init(void) {
+  printk(KERN_ALERT "Init namedpipe sucessfully.\n");
+  misc_register(&pipe);
   return 0;
 }
 
-static void __exit my_exit(void) {
-  misc_deregister(&my_misc_device);
-  printk(KERN_ALERT "Exit mytime.\n");
+static void __exit pipe_exit(void) {
+  misc_deregister(&pipe);
+  printk(KERN_ALERT "Exit namedpipe.\n");
 }
 
 
 
-module_init(my_init);
-module_exit(my_exit);
+module_init(pipe_init);
+module_exit(pipe_exit);
